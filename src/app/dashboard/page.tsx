eaaedd0106c8c29be8ccee3e54b661bd456ec9bf
Axel Pointud <axel@leedpulse.com>
2025-06-26 17:17:02 +0200

    feat: add user authentication to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,13 @@
+import { getUser } from "@/lib/auth-server";
+import { unauthorized } from "next/navigation";
 import React from "react";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const user = await getUser();
+
+  if (!user) {
+    return unauthorized();
+  }
+
   return <div>page</div>;
 }
